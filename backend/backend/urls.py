9846a4e1abe65c9f3e7a0114fc81a5ca9f6d98eb
sachin-error <vikneshsachin705@gmail.com>
2026-03-04 15:49:37 +0530

    backendmsgwrite

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("backend run successfully")
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
       path('api/', include('music.urls')),
 ]
